5b99c3e1be46de0730571697ba83998cd7fc50ee
RGuda-star <gudarish@msu.edu>
2026-02-16 10:44:00 -0500

    Changing wisc data file

--- read_in_wisc_data.py
+++ read_in_wisc_data.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data_1 = pd.read_csv('nwaa_data_1_of_4.csv')
-data_2 = pd.read_csv('nwaa_data_2_of_4.csv')
-data_3 = pd.read_csv('nwaa_data_3_of_4.csv')
-data_4 = pd.read_csv('nwaa_data_4_of_4.csv')
+data_1 = pd.read_csv('nwaa_data_wisc_PSC1.csv')
+data_2 = pd.read_csv('nwaa_data_wisc_PSC2.csv')
+data_3 = pd.read_csv('nwaa_data_wisc_PSC3.csv')
+data_4 = pd.read_csv('nwaa_data_wisc_PSC4.csv')
 
-df = pd.concat([data_1, data_2, data_3, data_4], ignore_index = True)
-print(df.head())
+df1 = pd.concat([data_1, data_2, data_3, data_4], ignore_index = True)
+#print(df1.head())
+
+data_5 = pd.read_csv('nwaa_data_wisc_HSM1.csv')
+data_6 = pd.read_csv('nwaa_data_wisc_HSM2.csv')
+data_7 = pd.read_csv('nwaa_data_wisc_HSM3.csv')
+data_8 = pd.read_csv('nwaa_data_wisc_HSM4.csv')
+
+df2 = pd.concat([data_5, data_6, data_7, data_8], ignore_index = True)
+#print(df2.head())
+
+df = pd.merge(df2, df1)
+print(df[0:20])

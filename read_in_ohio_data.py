f0a3d91bd6a9d105c2f7504d65852bd45ef7b88d
RGuda-star <gudarish@msu.edu>
2026-02-16 10:44:42 -0500

    Changing ohio data file

--- read_in_ohio_data.py
+++ read_in_ohio_data.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data_1 = pd.read_csv('nwaa_data_1_of_3.csv')
-data_2 = pd.read_csv('nwaa_data_2_of_3.csv')
-data_3 = pd.read_csv('nwaa_data_3_of_3.csv')
+data_1 = pd.read_csv('nwaa_data_ohio_PSC1.csv')
+data_2 = pd.read_csv('nwaa_data_ohio_PSC2.csv')
+data_3 = pd.read_csv('nwaa_data_ohio_PSC3.csv')
 
-df = pd.concat([data_1, data_2, data_3], ignore_index = True)
-print(df.head())
+df1 = pd.concat([data_1, data_2, data_3], ignore_index = True)
+#print(df1.head())
+
+data_4 = pd.read_csv('nwaa_data_ohio_HSM1.csv')
+data_5 = pd.read_csv('nwaa_data_ohio_HSM2.csv')
+data_6 = pd.read_csv('nwaa_data_ohio_HSM3.csv')
+
+df2 = pd.concat([data_4, data_5, data_6], ignore_index = True)
+#print(df2.head())
+
+df = pd.merge(df2, df1)
+print(df[0:14])
